8c068fcd5d480e412e5f6774d1b3cbe337612148
Christian Salway <ccsalway@yahoo.co.uk>
2019-09-10 13:17:51 +0100

    Update variables.tf

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -7,6 +7,7 @@ locals {
     config = {
       aws_default_region = var.aws_default_region
       state_bucket_name  = var.state_bucket_name
+      account_access_role_name = "AdminAccessRole"
     }
   }
 }
